3160aa0508f1fea06393047d85862eac31c385db
Zshan0 <zeeshan060801@gmail.com>
2021-11-16 02:53:40 +0530

    Made base class for GNN that uses GNNLayer. Need to add train/test code and test.

--- src/models/GNN.py
+++ src/models/GNN.py
@@ -4,77 +4,92 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 
+from GNNLayer import GNNLayer
+
 import sys
 
 sys.path.append("..")
 from graph import *
 
 
-class GNNLayer(nn.Module):
-    def __init__(self, input_dim: int, output_dim: int, is_concat: bool):
+class GNN(nn.Module):
+    def __init__(
+        self,
+        input_dim: int,
+        num_layers: int,
+        output_dim: int,
+        is_concat: bool = False,
+    ):
         """
-        Class for a general layer of a graph neural network that is based on the
-        Graph class defined. Serves as a base class for different types of GNN
-        layers.
+        General class for a graph neural network that is based on the Graph
+        class defined. Serves as a base class for different types of GNNs.
 
         Inputs:
             input_dim: Size of input features of the nodes.
+            num_layers: Number of GNN layers to be considered.
             output_dim: Final output size of the embedding.
             is_concat: If the combine function involves concatenation.
         """
-        super(GNNLayer, self).__init__()
+        super(GNN, self).__init__()
 
         self.input_dim = input_dim
+        self.num_layers = num_layers
         self.output_dim = output_dim
         self.is_concat = is_concat
 
-        if is_concat:
-            self.W = nn.Parameter(torch.FloatTensor(2 * output_dim, input_dim))
-        else:
-            self.W = nn.Parameter(torch.FloatTensor(output_dim, input_dim))
+        self.layers = nn.ModuleList()
+        self.layers.append(GNNLayer(input_dim, output_dim, is_concat))
+        # The output of ith layer will be passed to (i + 1)th layer
+        for i in range(1, num_layers):
+            self.layers.append(GNNLayer(output_dim, output_dim, is_concat))
 
-        # weight initialization
-        for param in self.parameters():
-            nn.init.xavier_uniform_(param)
-
-    def aggregate(self, H):
+    def combine_batch(self, graph_batch):
         """
-        Function that is overloaded by children classes. The base class
-        will use graphSAGE aggergate function which is:
-            MAX(ReLU(W \times h_u^{k - 1}), \forall u N(v))
+        Sets the index for all the nodes together so that the functions
+        can be run together faster.
 
         Inputs:
-            H: row-wise 2D matrix of neighbouring features after COMBINE.
+            graph_batch: list of graphs
         """
-        combined = F.relu(self.W.mm(H.t())).t()
-        print(combined)
-        # taking max of each column i.e feature
-        return torch.max(combined, dim=0).values
+        max_degree = max([graph.max_degree for graph in graph_batch])
 
-    def combine(self, H, a):
+        total_nodes = sum([len(graph) for graph in graph_batch])
+
+        cur_num = 0
+
+        neighbour_list = np.full((total_nodes, max_degree), -1)
+        for graph_num, graph in enumerate(graph_batch):
+            nodes = len(graph)
+            for ind, neighbours in enumerate(graph.neighbours):
+                if not self.concat:
+                    neighbour_list[cur_num + ind][
+                        0 : len(neighbours) + 1
+                    ] = neighbours + [ind]
+                else:
+                    neighbour_list[cur_num + ind][
+                        0 : len(neighbours)
+                    ] = neighbours
+
+            cur_num += nodes
+
+        return torch.Tensor(neighbour_list)
+
+    def forward(self, graph_batch: List[Graph]):
         """
-        Function that is overloaded by children classes. The base class
-        will use graphSAGE combine function which is:
-            h = [h, a]
+        Common function that applies each layer sequentially and stores all the
+        embeddings.
 
         Inputs:
-            H: row-wise 2D matrix of neighbouring features after COMBINE.
-            a: node feature row-vector.
+            graph_batch: list of graphs
         """
-        if self.is_concat:
-            return torch.cat([H, a], 1)
-        else:
-            return H
+        # dim(graph_features) = batch x num_neighbour x dim(node_features)
+        graph_features = torch.cat(
+            [graph.node_features for graph in graph_batch], 0
+        )
 
-    def forward(self, node_features, neigh_features):
-        """
-        Common function which applies combine and aggregate in sequence. Need
-        not be overloaded.
+        combined_neighbours = self.combine_batch(graph_batch)
 
-        Inputs:
-            node_features: row-vector of size 1 x input_dim.
-            neigh_features: 2D matrix of size num_neighbors x input_dim
-
-        Returns: [num_neighbors x output_dim] node embedding
-        """
-        return self.aggregate(self.combine(neigh_features, node_features))
+        hidden_features = [graph_features]
+        h = graph_features
+        for layer in range(self.num_layers - 1):
+            h = self.layers[layer](h)
