cc3dfa3ced694aca22f2d0dc7371a847aeb07e88
Zshan0 <zeeshan060801@gmail.com>
2021-11-19 12:02:47 +0530

    Writing train & test.

--- src/run.py
+++ src/run.py
@@ -13,13 +13,23 @@ import torch.optim as optim
 import numpy as np
 
 from tqdm import tqdm
+
+from data_handling import parse_dataset, k_fold_splitter
+from models.GNN import GNN
+
+criterion = nn.CrossEntropyLoss()
+
+from tqdm import tqdm
 from data_handling import parse_dataset, k_fold_splitter
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="SMAI project team 30")
     parser.add_argument(
-        "--dataset", type=str, default="MUTAG", help="name of dataset (default: MUTAG)"
+        "--dataset",
+        type=str,
+        default="MUTAG",
+        help="name of dataset (default: MUTAG)",
     )
     parser.add_argument(
         "--batch_size",
@@ -106,10 +116,87 @@ def parse_arguments():
     return parser.parse_args()
 
 
+def train(args, model, device, train_graphs, optimizer, epoch):
+    model.train()
+
+    total_iters = args.iters_per_epoch
+    pbar = tqdm(range(total_iters), unit='batch')
+
+    loss_accum = 0
+    for pos in pbar:
+        selected_idx = np.random.permutation(len(train_graphs))[
+            : args.batch_size
+        ]
+
+        batch_graph = [train_graphs[idx] for idx in selected_idx]
+        output = model(batch_graph)
+
+        labels = torch.LongTensor([graph.label for graph in batch_graph]).to(
+            device
+        )
+
+        # compute loss
+        loss = criterion(output, labels)
+
+        # backprop
+        if optimizer is not None:
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        loss = loss.detach().cpu().numpy()
+        loss_accum += loss
+
+        # report
+        pbar.set_description('epoch: %d' % (epoch))
+
+    average_loss = loss_accum / total_iters
+    print("loss training: %f" % (average_loss))
+
+    return average_loss
+
+
+def test(args, model, device, train_graphs, test_graphs, epoch):
+    model.eval()
+
+    output = pass_data_iteratively(model, train_graphs)
+    pred = output.max(1, keepdim=True)[1]
+    labels = torch.LongTensor([graph.label for graph in train_graphs]).to(
+        device
+    )
+    correct = pred.eq(labels.view_as(pred)).sum().cpu().item()
+    acc_train = correct / float(len(train_graphs))
+
+    output = pass_data_iteratively(model, test_graphs)
+    pred = output.max(1, keepdim=True)[1]
+    labels = torch.LongTensor([graph.label for graph in test_graphs]).to(device)
+    correct = pred.eq(labels.view_as(pred)).sum().cpu().item()
+    acc_test = correct / float(len(test_graphs))
+
+    print("accuracy train: %f test: %f" % (acc_train, acc_test))
+
+    return acc_train, acc_test
+
+
 def main():
     args = parse_arguments()
-    ##
-    pass
+    torch.manual_seed(0)
+    np.random.seed(0)
+    device = (
+        torch.device("cuda:" + str(args.device))
+        if torch.cuda.is_available()
+        else torch.device("cpu")
+    )
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(0)
+
+    graphs, num_classes = parse_dataset(args.dataset, args.degree_as_tag)
+
+    ##10-fold cross validation.
+    train_graphs, test_graphs = k_fold_splitter(
+        graphs, args.seed, args.fold_idx
+    )
+    print(num_classes, len(train_graphs), len(test_graphs))
 
 
 if __name__ == "__main__":
