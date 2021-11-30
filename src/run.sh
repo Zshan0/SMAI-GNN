1e8b21eb0e28074514b51a7845548e99af95f7fc
Pulak Malhotra <56169176+PulakIIIT@users.noreply.github.com>
2021-11-30 22:04:52 +0530

    more progress

--- src/run.sh
+++ src/run.sh
@@ -1,10 +1,10 @@
 dataset=PROTEINS
-name=GNN_MAX
+name=GIN
 folder_name="$name-$dataset"
 mkdir $folder_name
 for fold_idx in {0..9}
 do
-  python3 run.py --dataset $dataset --epochs 200 --fold_idx $fold_idx --output_folder $folder_name --name $name
+  python3 run.py --dataset $dataset --epochs 20 --fold_idx $fold_idx --output_folder $folder_name --name $name --mlp_layers 5
 done
 zip -r "$folder_name.zip" $folder_name
 # from google.colab import files
